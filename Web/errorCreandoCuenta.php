62b83fb33be90a32d11678e71c1ac0a9c0724dd1
jkatan <jkatan@users.noreply.github.com>
2017-12-07 18:07:54 -0300

    mail confirmation update

---
+++ Web/errorCreandoCuenta.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'lib/swift_required.php';
+include('header.php');
+?>
+    <h1>Error creando cuenta</h1>
+<?php include("footer.php"); ?>
--- Web/registrarPersonaAction.php
+++ Web/registrarPersonaAction.php
@@ -1,20 +1,39 @@
 <?php
   require_once(__DIR__.'/backend/utils.php');
+//  require_once(__DIR__.'lib/swift_required.php');
   $result = insertPersonFromPOST($_POST);
-  if($result) {
-    header( "refresh:3;url=index.php" );
-  } else {
-    header( "refresh:3;url=registrarPersona.php" );
-  }
   include("header.php");
 ?>
-<h1>Registrar Persona</h1>
+<h1>Procesando envío de información...</h1>
     <?php
       if($result) {
-        echo "Se cre&oacute; correctamente su cuenta Personal!</br>";
-        echo "Espere unos segundos a ser redireccionado...";
+        $username = $POST['username'];
+        $password = $POST['pass'];
+        $email = $POST['email'];
+
+        //Confirmation key
+        $key = $username . $password . $email;
+        $key = md5($key);
+
+      // Creo un transport de una cuenta de hotmail
+      $transport =  Swift_SmtpTransport::newInstance()
+          ->setUsername('goWork@outlook.com')->setPassword('passwordDeGowork')
+          ->setHost('smtp-mail.outlook.com')
+          ->setPort(587)->setEncryption('tls');
+
+        // Create the message
+        $message = Swift_Message::newInstance();
+        $message->setTo(array($email => $username));
+        $message->setSubject("Confirmation key - GoWork");
+        $message->setBody($key);
+        $message->setFrom("goWork@outlook.com", "GoWork");
+
+        // Send the email
+        $mailer = Swift_Mailer::newInstance($transport);
+        $mailer->send($message);*/
+
       } else {
-        echo "Error al crear la cuenta!";
+        header( "refresh:3;url=errorCreandoCuenta.php" );
       }
     ?>
 <?php include("footer.php"); ?>
